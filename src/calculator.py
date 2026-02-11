436024a6b76111ee401e5e5b43d0a0ecfd0daade
HunterB433 <hmb433@nau.edu>
2026-02-11 15:08:52 -0700

    Calculator

---
+++ src/calculator.py
@@ -0,0 +1,10 @@
+#calculator
+
+def add(a, b):
+    return a+b
+
+def subtract(a, b):
+    return a-b
+
+def divide(a, b):
+    return a/b
---
+++ tests/test_calculator.py
@@ -0,0 +1,23 @@
+#test_calculator
+
+import unittest
+from calculator import add, subtract, divide
+
+class TestCalculator(unittest.TestCase):
+   def test_add(self):
+    self.assertEqual(add(2, 3), 5)
+    self.assertEqual(add(-1, 1), 0)
+    self.assertEqual(add(-1, -1), -2)
+def test_subtract(self):
+    self.assertEqual(subtract(4, 2), 2)
+    self.assertEqual(subtract(1, -1), 0)
+    self.assertEqual(subtract(0, 2), -2)
+def test_divide(self):
+    self.assertEqual(divide(8, 4), 2)
+    self.assertEqual(divide(0, 2), 0)
+    self.assertEqual(divide(4, -2), -2)
+    self.assertEqual(divide(-4, 2), -2)
+    self.assertEqual(divide(-4, -2), 2)
+
+if __name__ == '__main__':
+   unittest.main()
